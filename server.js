d769fa18dff39078b7de68b88f8805579027e96b
andrijaitis <andrijaitis92@gmail.com>
2018-11-05 13:49:19 +0100

    ready for heroku

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/coachgo_front'));
+app.use(express.static(__dirname + '/dist/couchgo'));
+console.log(__dirname + '/dist/couchgo');
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/coachgo_front/index.html'));
+res.sendFile(path.join(__dirname+'/dist/couchgo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
