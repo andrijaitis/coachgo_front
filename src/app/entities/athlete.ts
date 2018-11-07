3ee2674998b0e5b53aefcba5c2c5e1523f0b8ff9
andrijaitis <andrijaitis92@gmail.com>
2018-11-07 06:20:24 +0100

    coach dash

---
+++ src/app/entities/athlete.ts
@@ -0,0 +1,8 @@
+export class Athlete {
+      public email: String;
+      public activity: String;
+      public firstName: String;
+      public lastName: String;
+      public age: String;
+      public height: String;
+  }
