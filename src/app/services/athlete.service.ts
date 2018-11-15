6581f41842500fdf45283a14a5c4d661547ece97
andrijaitis <andrijaitis92@gmail.com>
2018-11-15 05:10:01 +0100

    test lso

--- src/app/services/athlete.service.ts
+++ src/app/services/athlete.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Athlete } from '../entities/athlete';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { catchError } from 'rxjs/operators';
 import { UserService } from './user.service';
-const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json',
+const httpOptions = {
+  headers: new HttpHeaders({
+  'Content-Type': 'application/json',
  'Authorization': 'Bearer ' + localStorage.getItem('token')
 })};
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AthleteService {
-  //  token = this.userService.getToken();
-  // Authorization = {'Authorization': 'Bearer ' + this.userService.getToken()};
+export class AthleteService implements OnInit {
+  ngOnInit(): void { }
+ 
 
   addAthlete(athlete: Athlete): Observable<Athlete> {
     localStorage.setItem('token', this.userService.getToken());
