5cc5f8bd6ed5479f85891e5efb33a3f7e34f4f5f
andrijaitis <andrijaitis92@gmail.com>
2018-11-15 03:11:38 +0100

    test ls

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -40,6 +40,7 @@ export class LoginComponent implements OnInit {
 
 
   onSubmitLogin(loginForm) {
+    localStorage.setItem('userId', 'dxfcgvhbjkl');
     // localStorage.clear();
     if (loginForm.valid) {
       const user: User = loginForm.value;
